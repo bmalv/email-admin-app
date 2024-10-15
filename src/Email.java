c5475b06386d5b580d0faac84d58ce15bfd31191
bmalv <74278503+bmalv@users.noreply.github.com>
2024-10-15 15:05:36 -0500

    prompting user and setting department field

--- src/Email.java
+++ src/Email.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -11,7 +13,35 @@ public class Email {
         this.lastName = lastName;
         this.firstName = firstName;
         System.out.println("EMAIL CREATED: " + this.firstName + " " + this.lastName);
+
+        //call a method asking for the department and returns the department
+        this.departmentName = setDepartment();
+        System.out.println("Department: " + this.departmentName);
+
     }
+
+    //Ask for department
+    private String setDepartment(){
+        System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code:");
+        Scanner in = new Scanner(System.in);
+        int departmentChoice = in.nextInt();
+        if(departmentChoice == 1){
+            return "sales";
+        }else if(departmentChoice == 2){
+            return "dev";
+        }else if(departmentChoice == 3){
+            return "acct";
+        }
+        return "";
+    }
+
+    //Generate a random password
+
+    //Set the mailbox capacity
+
+    //Set the alternate email
+
+    //Change the password
 
 
 }
